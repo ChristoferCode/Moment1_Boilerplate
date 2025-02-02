ac09bc9406e9c5cb57c8f68867f8947c8cf60677
Christofer Hansson <christofer_hansson@hotmail.com>
2025-02-02 08:59:49 +0100

    Ändrade menyer med mera glow och fixad startsida med rubrik, text och logotyper

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 ## Moment 1 - Automatisering & publicering
 Denna uppgift går ut på att:
   * *skapa en "boilerplate" för webbutveckling som jag kan ha som mall för kommande webbplatser jag skapar*
-  * *att jag skapar en automatiserad utvecklingsmiljö baserad på Git och NodeJs så att jag endast via uppdatering till Github kan publicera mina uppdateringar på min webbplats*
-  * *att jag använder Parcel för att bygga och förenkla min webbplats. CSS, HTML och JavaScript minifieras och transpileras för att minska filstorlekar och ladditd av min webbplats. Även bildfiler optimeras automatiskt.*
+  * *jag skapar en automatiserad utvecklingsmiljö baserad på Git och NodeJs så att jag endast via uppdatering till Github kan publicera mina uppdateringar på min webbplats*
+  * *jag använder Parcel för att bygga och förenkla min webbplats. CSS, HTML och JavaScript minifieras och transpileras för att minska filstorlekar och laddtid av min webbplats. Även bildfiler optimeras automatiskt.*
 
 Detta repository är kopplat till mitt Netlify-konto och så fort main-branch uppdateras så uppdateras min webbplats som är publicerad via Netlify. Under tiden jag utvecklar min webbplats jobbar jag mot dev-branch för versionshantering men detta publiceras alltså inte. När jag vill publicera så mergar jag ihop dev och main så att main syncar mina senaste uppdateringar i dev.
 
--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ let openMenuEl = document.getElementById("open-menu");
 let closeMenuEl = document.getElementById("close-menu");
 let menuEl = document.getElementById("menu");
 
+
 let menutextopenEl = document.querySelector(".menutextopen");
 let bar1El = document.querySelector(".bar1");
 let bar2El = document.querySelector(".bar2");
@@ -16,9 +17,15 @@ let bar2closeEl = document.querySelector(".bar2close");
 let bar3closeEl = document.querySelector(".bar3close");
 
 
+openMenuEl.addEventListener("mouseover", hoverOpen);
+closeMenuEl.addEventListener("mouseover", hoverClose);
+
+openMenuEl.addEventListener("mouseout", hoverOpen2);
+closeMenuEl.addEventListener("mouseout", hoverClose2);
 
 openMenuEl.addEventListener("click", toggleMenu);
 closeMenuEl.addEventListener("click", toggleMenu);
+
 
 
 
@@ -36,6 +43,7 @@ function toggleMenu() {
         menutextopenEl.style.animationPlayState = "running";
         menutextopenEl.innerHTML = "Stäng";
 
+
         closeMenuEl.style.animationPlayState = "paused";
         bar1closeEl.style.animationPlayState = "paused";
         bar2closeEl.style.animationPlayState = "paused";
@@ -43,9 +51,10 @@ function toggleMenu() {
         menutextcloseEl.style.animationPlayState = "paused";
       
 
+        showMenu()
+
 
     } else if (window.getComputedStyle(menuEl).display === "flex" && window.matchMedia("(max-width: 800px)").matches) {
-        menuEl.style.display = "none";
         openMenuEl.style.display = "none";
         closeMenuEl.style.display = "flex";
 
@@ -55,12 +64,15 @@ function toggleMenu() {
         bar3closeEl.style.animationPlayState = "running";
         menutextcloseEl.style.animationPlayState = "running";
         menutextcloseEl.innerHTML = "Meny";
+  
 
         openMenuEl.style.animationPlayState = "paused";
         bar1El.style.animationPlayState = "paused";
         bar2El.style.animationPlayState = "paused";
         bar3El.style.animationPlayState = "paused";
         menutextopenEl.style.animationPlayState = "paused";
+
+        hideMenu()
 
     } 
 
@@ -106,7 +118,48 @@ function noHamburger() {
 }
 
 
+function showMenu() {
+    menuEl.style.animationPlayState = "running";
+}
 
+function hideMenu() {
+    menuEl.style.animationName = "hidemenu";
+    menuEl.style.animationPlayState = "running";
+
+    setTimeout(closeMenu, 500)
+}
+
+function closeMenu() {
+    menuEl.style.display = "none";
+    menuEl.style.animationName = "showmenu";
+
+}
+
+
+function hoverOpen() {
+    if (window.getComputedStyle(menuEl).display === "none") {
+        openMenuEl.style.boxShadow ="0px 2px 8px white";
+    } else {
+        openMenuEl.style.boxShadow ="0px 2px 8px red";
+    }   
+
+}
+
+function hoverClose() {
+   if (window.getComputedStyle(openMenuEl).display === "none") {
+        closeMenuEl.style.boxShadow ="0px 2px 8px white";
+    } else {
+        closeMenuEl.style.boxShadow ="0px 2px 8px red";
+    }
+}
+
+function hoverOpen2() {
+        openMenuEl.style.boxShadow ="none";
+    }
+    
+    function hoverClose2() {
+        closeMenuEl.style.boxShadow ="none";
+    }
 
 // function openMenu() {
 
