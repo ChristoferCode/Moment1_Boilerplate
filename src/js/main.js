d4e690e9551f4b00532a5256faade61849696980
Christofer Hansson <christofer_hansson@hotmail.com>
2025-02-02 12:05:14 +0100

    städa upp js-filen

--- src/js/main.js
+++ src/js/main.js
@@ -28,7 +28,6 @@ closeMenuEl.addEventListener("click", toggleMenu);
 
 
 
-
 function toggleMenu() {
 
     if (window.getComputedStyle(menuEl).display === "none" && window.matchMedia("(max-width: 800px)").matches) {
@@ -43,16 +42,13 @@ function toggleMenu() {
         menutextopenEl.style.animationPlayState = "running";
         menutextopenEl.innerHTML = "Stäng";
 
-
         closeMenuEl.style.animationPlayState = "paused";
         bar1closeEl.style.animationPlayState = "paused";
         bar2closeEl.style.animationPlayState = "paused";
         bar3closeEl.style.animationPlayState = "paused";
         menutextcloseEl.style.animationPlayState = "paused";
       
-
         showMenu()
-
 
     } else if (window.getComputedStyle(menuEl).display === "flex" && window.matchMedia("(max-width: 800px)").matches) {
         openMenuEl.style.display = "none";
@@ -65,7 +61,6 @@ function toggleMenu() {
         menutextcloseEl.style.animationPlayState = "running";
         menutextcloseEl.innerHTML = "Meny";
   
-
         openMenuEl.style.animationPlayState = "paused";
         bar1El.style.animationPlayState = "paused";
         bar2El.style.animationPlayState = "paused";
@@ -73,9 +68,7 @@ function toggleMenu() {
         menutextopenEl.style.animationPlayState = "paused";
 
         hideMenu()
-
     } 
-
     window.addEventListener("resize", noHamburger);
 }
 
@@ -98,23 +91,6 @@ function noHamburger() {
         menutextcloseEl.style.animationPlayState = "running";
         menutextcloseEl.innerHTML = "Meny";
     }
-
-
-
-
-    // if (window.matchMedia("(min-width: 800px)").matches) {
-    //     menuEl.style.display = "flex";
-    //     openMenuEl.style.display = "none";
-    //     closeMenuEl.style.display = "none";
-    // } else if (window.getComputedStyle(openMenuEl).display === "flex" && window.matchMedia("(max-width: 800px)").matches) {
-    //     menuEl.style.display = "none";
-    //     openMenuEl.style.display = "flex";
-    //     closeMenuEl.style.display = "none";
-    // } else if (window.getComputedStyle(closeMenuEl).display === "flex" && window.matchMedia("(max-width: 800px)").matches) {
-    //     menuEl.style.display = "flex";
-    //     openMenuEl.style.display = "none";
-    //     closeMenuEl.style.display = "flex";
-    // }
 }
 
 
@@ -132,7 +108,6 @@ function hideMenu() {
 function closeMenu() {
     menuEl.style.display = "none";
     menuEl.style.animationName = "showmenu";
-
 }
 
 
@@ -154,127 +129,14 @@ function hoverClose() {
 }
 
 function hoverOpen2() {
-        openMenuEl.style.boxShadow ="none";
-    }
+    openMenuEl.style.boxShadow ="none";
+}
     
-    function hoverClose2() {
-        closeMenuEl.style.boxShadow ="none";
-    }
-
-// function openMenu() {
-
-//     if (window.getComputedStyle(menuEl).display === "none" && window.matchMedia("(max-width: 800px)").matches) {
-//         menuEl.style.display = "flex";
-//         openMenuEl.style.animationPlayState = "running";
-//         bar1El.style.animationPlayState = "running";
-//         bar2El.style.animationPlayState = "running";
-//         bar3El.style.animationPlayState = "running";
-//         menutextopenEl.style.animationPlayState = "running";
-//         menutextopenEl.innerHTML = "Stäng";
-
-//     } else if (window.getComputedStyle(menuEl).display === "flex" && window.matchMedia("(max-width: 800px)").matches) {
-//         closeMenuEl.style.display = "flex";
-//         openMenuEl.style.display = "none";
-//     }
-// }
-
-
-// function closeMenu() {
-
-//     if (window.getComputedStyle(menuEl).display === "flex" && window.matchMedia("(max-width: 800px)").matches) {
-//         menuEl.style.display = "none";
-//         closeMenuEl.style.animationPlayState = "running";
-//         bar1closeEl.style.animationPlayState = "running";
-//         bar2closeEl.style.animationPlayState = "running";
-//         bar3closeEl.style.animationPlayState = "running";
-//         menutextcloseEl.style.animationPlayState = "running";
-//         menutextcloseEl.innerHTML = "Meny";
-
-//     } else if (window.getComputedStyle(menuEl).display === "none" && window.matchMedia("(max-width: 800px)").matches) {
-//         closeMenuEl.style.display = "none";
-//         openMenuEl.style.display = "flex";
-//     }
-// }
-
-// function resetMenu() {
-//     openMenuEl.style.animationPlayState = "paused";
-//     bar1El.style.animationPlayState = "paused";
-//     bar2El.style.animationPlayState = "paused";
-//     bar3El.style.animationPlayState = "paused";
-//     menutextopenEl.style.animationPlayState = "paused";
-
-//     openMenuEl.style.animationDirection = "reverse";
-//     bar1El.style.animationDirection = "reverse";
-//     bar2El.style.animationDirection = "reverse";
-//     bar3El.style.animationDirection = "reverse";
-//     menutextopenEl.style.animationDirection = "reverse";
-
-
-// }
-
-
-// function closeMenu() {
-
-//     if  (style.display === "flex" && window.matchMedia("(max-width: 800px)").matches) {
-//         menuEl.style.display = "none";
-//         openMenuEl.style.animationPlayState = "running";
-//         bar1El.style.animationPlayState = "running";
-//         bar2El.style.animationPlayState = "running";
-//         bar3El.style.animationPlayState = "running";
-//         menutextopenEl.style.animationPlayState = "running";
-//         menutextopenEl.innerHTML = "Meny";
-//     }
-
-//     setTimeout(resetMenuBack, 1000);
-// }
-
-
-
-
-// function resetMenu() {
-//     openMenuEl.style.animationPlayState = "paused";
-//     bar1El.style.animationPlayState = "paused";
-//     bar2El.style.animationPlayState = "paused";
-//     bar3El.style.animationPlayState = "paused";
-//     menutextopenEl.style.animationPlayState = "paused";
-
-//     openMenuEl.style.animationDirection = "normal";
-//     bar1El.style.animationDirection = "normal";
-//     bar2El.style.animationDirection = "normal";
-//     bar3El.style.animationDirection = "normal";
-//     menutextopenEl.style.animationDirection = "normal";
-
-//     openMenuEl.addEventListener("click", openMenu);
-// }
+function hoverClose2() {
+    closeMenuEl.style.boxShadow ="none";
+}
 
 
 
 
 
-// function toggleButton() {
-//     let styleOpen = window.getComputedStyle(openMenuEl);
-//     let styleClose = window.getComputedStyle(closeMenuEl);
-
-
-//     if (styleOpen.display === "flex") {
-//         openMenuEl.styleOpen.display = "none";
-//         closeMenuEl.styleClose.display = "flex";
-//     } else {
-//         openMenuEl.styleOpen.display = "flex";
-//         closeMenuEl.styleClose.display = "none";
-//     }
-// }
-
-
-
-
-// if (styleOpen.display === "flex" && window.matchMedia("(min-width: 800px)").matches) {
-//     menuEl.style.display = "flex";
-// } 
-
-
-// if (styleClose.display === "none") {
-//     closeMenuEl.styleClose.display = "flex";
-// } else {
-//     closeMenuEl.styleClose.display = "none";
-// }
